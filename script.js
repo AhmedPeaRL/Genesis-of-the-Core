317ca7a40dbf126e9a78488dc9ab33f9fa705fb0
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2025-04-30 03:46:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function activateCore() {
+  const container = document.querySelector('.core-container');
+  container.innerHTML = '<h1>Core Activated</h1><p>Transmission to the Source initiated...</p>';
+  container.style.boxShadow = '0 0 60px rgba(0,255,150,0.5)';
+}
